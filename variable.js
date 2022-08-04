8f30a357b15898f0d3c7c3bdd4a86e799124ead6
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-04 07:45:49 +0000

    variable practice

---
+++ variable.js
@@ -0,0 +1,51 @@
+// VARIABLE IN JAVASCRIPT
+
+// VAR  
+
+// numOne = 100;
+// numTwo = 200;
+// console.log(numOne + numTwo);
+// var numOne = 12;
+// var numTwo = 20;
+// var numTwo = 30;
+// var numTwo = 50;
+// console.log(numOne + numTwo);
+
+
+
+
+
+
+
+
+// LET 
+
+// console.log(numberOne + numberTwo);
+// let numberOne = 10;
+
+// let numberTwo = 20;
+// let numberOne = 30;
+
+// console.log(numberOne + numberTwo);
+
+// let myArray = ["1", "aryan", "3"];
+// myArray[0] = "kailash"; // you can change array element itme values
+
+// let myArray = ["kk", "rk"]; //redeclared error
+// console.log(myArray);
+
+
+
+
+
+
+
+
+
+// CONST
+
+const numOne = 10;
+
+const numTwo = 20;
+
+console.log(numOne + numTwo);

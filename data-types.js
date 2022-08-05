f744897c75333680d210c69ce2433a53314e4783
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-05 15:37:15 +0000

    data types practice

---
+++ data-types.js
@@ -0,0 +1,62 @@
+// DATA TYPES IN JAVASCRIPT
+
+// ============== NUMBER ================
+
+// let numberOne = 100;
+// let numberTwo = 200.45;
+
+// console.log(numberOne + numberTwo);
+
+
+
+
+
+
+
+
+
+
+// ================= STRING ===================
+
+// let firstName = "Kailash";
+// let lastName = "Kumar";
+
+// console.log(firstName + " " + lastName);
+
+
+
+
+
+
+
+
+
+
+
+// =================== BOOLEAN ==================
+
+// let correctCondition = false;
+// let wrongCondition = false;
+
+// console.log(correctCondition + wrongCondition);
+
+// let numberOne = 10;
+// let numberTwo = 20;
+// let numberThree = 10;
+
+// console.log(numberOne == numberThree);
+// console.log(numberOne == numberTwo);
+// console.log(numberOne < numberTwo);
+// console.log(numberOne > numberTwo);
+
+
+
+
+
+
+
+
+
+
+
+// ================== BIGINT ================

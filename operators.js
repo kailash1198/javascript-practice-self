4bd6d08e826f3f2aed60819a59e844f80cdcf42d
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-06 16:11:38 +0000

    operaors practice code

---
+++ operators.js
@@ -0,0 +1,141 @@
+// ============= OPERATORS IN JAVASCRIPT ================
+
+// =========Airthmatic Operators ============
+// let numberOne = 10;
+// let numberTwo = 30;
+
+// let addOperate = numberOne + numberTwo;s
+// console.log(addOperate);
+
+// let minusOperate = numberOne - numberTwo;
+// console.log(minusOperate);
+
+// let multiplyOperate = numberOne * numberTwo;
+// console.log(multiplyOperate);
+
+// console.log(numberOne ** numberTwo);
+
+// console.log(--numberOne);
+
+// console.log(++numberTwo);
+
+// console.log(numberOne--);
+
+// console.log(numberOne++);
+
+// =================== COMPARISON OPERATORS IN JAVASCRIPT ==========
+
+// let numberOne = 10;
+// let numberTwo = 30;
+
+// console.log(numberOne < numberTwo);
+
+// console.log(numberOne > numberTwo);
+
+// console.log(numberOne >= numberTwo);
+
+// console.log(numberOne <= numberTwo);
+
+// console.log(numberOne == numberTwo);
+
+// console.log(numberOne === numberTwo);
+
+// console.log(numberOne != numberTwo);
+
+// console.log(numberOne !== numberTwo);
+
+
+
+// ================== BITWISE OPERATORS =============
+// let numberOne = 5;
+// let numberTwo = 1;
+
+// console.log(numberOne & numberTwo);
+// console.log(numberOne ^ numberTwo);
+// console.log(numberOne << numberTwo);
+// console.log(numberOne >> numberTwo);
+// console.log(~numberOne);
+// console.log(numberOne | numberTwo);
+
+// let checkBitwise = 5 | 1;
+// console.log(checkBitwise);
+
+// checkBitwise = 10 << 1;
+// console.log(checkBitwise);
+
+
+
+// =============== LOGICAL OPERATORS ================
+// let numberOne = 5 < 10;
+// let numberTwo = 1 < 2;
+
+// console.log(numberOne && numberTwo);
+// console.log(numberOne | numberTwo);
+// console.log(!numberTwo);
+
+
+// ================ ASSIGNMENT OPERATORS =================
+// let numberOne = 5;
+// let numberTwo = 1;
+
+// numberOne += 1;
+// console.log(numberOne);
+
+// numberOne -= 2;
+// console.log(numberOne);
+
+// numberTwo *= 10;
+// console.log(numberTwo);
+
+// numberTwo %= 2;
+// console.log(numberTwo);
+
+// numberTwo /= 2;
+// console.log(numberTwo);
+
+
+
+
+
+
+
+
+// ============ TERNARY/CONDITIONAL OPERATORS IN JAVASCRIPT ===========
+// let numberOne = 5;
+// let numberTwo = 1;
+
+// let result = (numberOne <= 5) ? "True" : "False";
+
+// console.log(result);
+
+
+
+
+
+
+// =============== typeof operator ==============
+// let numberOne = 10;
+// let numberTwo = 20;
+
+// let result = numberOne - numberTwo;
+// console.log(typeof result);
+
+
+
+
+// let firstName = "aryan";
+// let lastName = "kumar";
+// console.log(typeof firstName);
+
+
+
+
+
+// =============== special operator ==============
+//  delete
+// in
+// new
+// this
+//instanceof
+//void
+// comma

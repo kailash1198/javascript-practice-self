8bbcb3f4562ec3cc13cf6aafdde23e858361b194
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-08 02:07:33 +0000

    loops in js practice

---
+++ loops.js
@@ -0,0 +1,110 @@
+// ============= LOOPS IN JS ==============
+
+// ============ WHILE LOOP =============
+
+// let numOne = 1;
+
+// while (numOne <= 10) {
+//     console.log(`The numOne value is: ${numOne}`);
+//     console.log(numOne ** 2);
+//     numOne++;
+// }
+
+
+
+
+
+
+
+
+// ================= do while loop ================
+
+// let numTwo = 1;
+
+// do {
+//     console.log(numTwo);
+//     numTwo++;
+// } while (numTwo <= 20);
+
+
+
+
+
+
+
+
+
+
+// ================ for loop =====================
+
+// let numThree = 1;
+
+// for (numThree; numThree <= 30; numThree++) {
+//     console.log(numThree);
+// }
+
+
+
+
+
+
+
+
+// =============== for in loop ===============
+
+// const foodIAte = {
+//     breakfast: "eggs",
+//     lunch: "salad",
+//     dinner: "pizza"
+// };
+// for (const meal in foodIAte) {
+//     console.log(`For ${meal}, I ate ${foodIAte[meal]}.`);
+// };
+
+
+
+// const studentData = {
+//     name: "kailash",
+//     age: 23,
+//     location: "Banglore"
+// }
+
+// for (const data in studentData) {
+//     console.log(`for ${data}, my data is ${studentData[data]}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================== for of loop =====================
+
+// let myArray = [10, 20, 30];
+
+// for (const value of myArray) {
+//     console.log(value);
+// }
+
+
+
+// let myString = "kailash";
+
+// for (const value of myString) {
+//     console.log(value)
+// }
+
+
+// let stringArray = ["kailash", "kumar"];
+
+// for (const value of stringArray) {
+//     console.log(value);
+// }

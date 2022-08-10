65dc7fb17bbdd60ebdd5c5b81be32f807a4e7222
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-10 02:27:36 +0000

    CONDITIONAL STATEMENT PRACTICE DONE

---
+++ if-else.js
@@ -0,0 +1,50 @@
+// ============ if else statement ===========
+
+// let numOne = 10;
+// let numTwo = 100;
+
+// if (numOne <= numTwo) {
+//     let sumTotal = numOne + numTwo;
+//     console.log(sumTotal);
+// } else {
+//     console.log("num one not satisfied num two variables");
+// }
+
+// =============== else if statement==========
+
+// let age = 28;
+
+// if (age == 18) {
+//     console.log("you are eligible for voting");
+// } else if (age < 18) {
+//     console.log("Soory, You are under age");
+// } else if (age > 18) {
+//     console.log("You are our first priorities");
+// } else {
+//     console.log("Soory, You cannot processd");
+// }
+
+// =============== nested if else statement ======
+
+// let userName = "kailash1198";
+// let userPassword = 12345;
+
+// if (userName === "kailash1198") {
+//     if (userPassword == 12345) {
+//         console.log("Log in success");
+//     } else {
+//         console.log("Your password not correct, Please try again");
+//     }
+// } else {
+//     console.log("Your userName not correct, Please try again");
+// }
+
+
+
+
+
+
+
+/* =================================================================
+            ADVANCE PRACTICE FOR IF ELSE STATEMENT EXPERT
+   =================================================================*/

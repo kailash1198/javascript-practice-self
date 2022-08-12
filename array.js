2a7b31c0c89776981fe9e9b82b4997cba9691dd0
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-12 17:32:13 +0000

    console basic practice

---
+++ array.js
@@ -0,0 +1,12 @@
+// ============== ARRAY IN JAVASCRIPT ============
+const car = ["maruti", "tata", "toyota", "bmw"];
+console.log(car);
+
+console.log(car.length)
+
+car[0] = "volvo1";
+console.log(car);
+console.log(car[1]);
+
+const firstName = new Array("kailash", "aryan", "singh");
+console.log(firstName);

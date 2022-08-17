9109d818ed1f36b44d9de1eedccd820a3fdf1233
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-17 10:46:52 +0000

    arrays practice

--- array.js
+++ array.js
@@ -1,12 +1,95 @@
-// ============== ARRAY IN JAVASCRIPT ============
-const car = ["maruti", "tata", "toyota", "bmw"];
-console.log(car);
+/*===================================================== 
+               💕💕 ARRAY IN JAVASCRIPT 💕💕
+=======================================================*/
 
-console.log(car.length)
 
-car[0] = "volvo1";
-console.log(car);
-console.log(car[1]);
 
-const firstName = new Array("kailash", "aryan", "singh");
-console.log(firstName);
+
+// const car = ["maruti", "tata", "toyota", "bmw"];
+// console.log(car);
+
+// console.log(car.length)
+
+// car[0] = "volvo1";
+// console.log(car);
+// console.log(car[1]);
+
+// const firstName = new Array("kailash", "aryan", "singh");
+// console.log(firstName);
+
+// const myArrayName = new Array(3);
+// console.log(myArrayName);
+
+// myArrayName[0] = "banana";
+// myArrayName[1] = "apple";
+// myArrayName[2] = "guava";
+
+// console.log(myArrayName);
+
+// for (let i = 0; i < myArrayName.length; i++) {
+//     console.log(myArrayName[i]);
+// }
+
+// const mySecondArray = ["banana", "apple", "orange"];
+// for (let i = 0; i < mySecondArray.length; i++) {
+//     console.log(mySecondArray[i]);
+// }
+
+// const [bananaArray, appleArray] = ["banana", "apple"];
+
+// console.log(bananaArray);
+// console.log(appleArray);
+
+// const thirdArray = [1, 2, 3, 4, 5];
+// console.log(thirdArray.length);
+
+
+
+
+/*======================================================
+            ❤️❤️ARRAY METHOD❤️❤️ 
+=====================================================*/
+
+
+
+// -----------X------👉👉PUSH METHOD ----------X-----------
+
+// const firstArray = ["banana", "apple", "orange", "guava"];
+// firstArray.push("coconut");
+// firstArray.push("papaya", "grapes");
+// console.log(firstArray);
+
+// ----------X----- 👉👉POP METHOD ----------X---------------
+
+// const secondArray = ["kailash", " aryan", "singh", "laxmi"];
+// secondArray.pop();
+
+// console.log(secondArray);
+
+// ----------X-----👉👉 UNSHIFT METHOD ----------X---------------
+
+// const thirdArray = ["girls", "boys", "mens"];
+// thirdArray.unshift("women");
+
+// console.log(thirdArray);
+
+// ----------X----- 👉👉SHIFT METHOD ----------X---------------
+
+// const fourthArray = ["shyam", "banana", "apple"];
+
+// fourthArray.shift("grapes");
+
+// console.log(fourthArray);
+
+// ----------X-----👉👉 SLICE METHOD ----------X---------------
+
+// const fifthArray = [1, 2, 3, "kailash"];
+
+// const sliceOut = fifthArray.slice(0, 3);
+
+// console.log(fifthArray);
+// console.log(sliceOut);
+
+// ----------X----- 👉👉CONCAT METHOD ----------X---------------
+
+const myArray = ["kailash", "kumar", "aryan"];

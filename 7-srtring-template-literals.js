a2d65a7b80d2031742aecff9f4cf5032a5951b75
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-19 00:40:59 +0000

    practice type conversion and string literals

---
+++ 7-srtring-template-literals.js
@@ -0,0 +1,6 @@
+// ============== STRING AND TEMPLATE LITERALS ===========
+
+let firstName = "Kailash";
+let lastName = "Kumar";
+
+console.log(`My name is ${firstName} and my last name is ${lastName}`);
---
+++ 8-type-conversion.js
@@ -0,0 +1,13 @@
+// =========== TYPE CONVERSION ===========
+// original value are not converted in type conversion. 
+// Manual type conversion 
+// The type conversion only used in three data types which are String, number, and boolean. 
+
+let myNumber = "10"; //string data types
+
+let numberTwo = 10;
+
+console.log(Number(myNumber) + numberTwo);
+
+// ============ TYPE COERCION ========= 
+// AUTOMATIC convert type to another type according to data types

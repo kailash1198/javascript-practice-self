a6180cbcfccd8cd1cdd97f3aa0a031f3e57bc219
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-20 00:32:56 +0000

    switch statements practice and understanding

---
+++ 10-switch-statements.js
@@ -0,0 +1,21 @@
+// =========== SWITCH STATEMENT IN JAVASCRIPT ===========
+/*
+let numOne = 20;
+
+switch (numOne) {
+    case 1:
+        console.log("Number one value is 1");
+        break;
+    case 2:
+        console.log("Number one value is 2");
+        breakl;
+    case 3:
+        console.log("Number one value is 3")
+        break;
+    case 4:
+        console.log("NMumber one value is 4");
+        break;
+    default:
+        console.log("Please enter valid number or valid input");
+}
+*/

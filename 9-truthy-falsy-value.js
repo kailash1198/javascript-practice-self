56e41c38584b7fcf566c18772ce3769626fe7c27
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-20 00:19:00 +0000

    truthy and falsy value practice and understanding

---
+++ 9-truthy-falsy-value.js
@@ -0,0 +1,34 @@
+// ========= TRUTHY AND FALSY VALUE IN JAVASCRIPT==========
+// Falsy value list: falsy, NAN, undefined, "", '', 0
+// if you want check truthy or falsy then you can use 👉 Boolean() function in your code 
+
+// let a = 10;
+// let b = 20;
+// let c = a == 20;
+
+// console.log(c);
+// console.log(Boolean(c));
+
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean(null));
+// console.log(Boolean(false));
+// console.log(Boolean(""));
+
+// --------- truthy value-----------
+// console.log(Boolean(20));
+// console.log(Boolean({}));
+
+
+
+// ===== examples =========== 
+
+// const money = 0;
+// if (money) {
+//     console.log("DO not spend at all");
+// } else {
+//     console.log("you should get a job");
+// }
+
+// because 0 is falsy value so then excute else statements
+// ===================================

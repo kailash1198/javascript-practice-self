74e1df26405e51d56a643ce2def59cce3f3e3d81
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-20 00:50:14 +0000

    practice and understanding of statement and expression in javascript

---
+++ 11-strict-mode.js
@@ -0,0 +1,21 @@
+// ============ JAVASCRIPT STRICT MODE ==========
+// bigger piece of or line of code is called statements.
+// let a = 10;
+// if (a <= 10) {
+//     console.log("a less then 10 ");
+// } else {
+//     console.log("a is not less then 10");
+// }
+
+
+
+
+
+
+
+
+
+
+// single line operation is called expression
+// let a = 3 + 4;
+// let c = 10 - 8;

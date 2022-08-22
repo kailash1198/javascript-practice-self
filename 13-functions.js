b849a17813a7ff61d2ea5eb40e711b1202f3d062
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-22 01:27:29 +0000

    function learning and practice

---
+++ 13-functions.js
@@ -0,0 +1,17 @@
+// =============== BASIC FUNCTION ===========
+
+function showMessege() {
+    let messege = "Hello world"; //local variables
+    alert(messege);
+}
+showMessege();
+
+
+function printName(firstName, lastName) {
+    alert(firstName + lastName);
+}
+
+let userName = "kailash";
+let secondName = "kumar";
+
+printName(userName, secondName);

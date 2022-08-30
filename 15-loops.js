b795f58b09a5b97a82609a82fec6d87d554b8fea
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-08-30 05:11:42 +0000

    loops practice in javascript

---
+++ 15-loops.js
@@ -0,0 +1,41 @@
+// =============== LOOPS IN JAVASCRIPT ============
+// If same code writing lots of time then waste your time you can use loops for saving time in coding.
+// -----x--------- Types of loops ------x---------
+
+// 1. while loops in javascript
+
+// let numOne = 1;
+
+// while (numOne <= 10) {
+//     console.log(numOne);
+//     numOne++;
+// }
+
+
+
+// while (numOne <= 20) {
+//     console.log(numOne);
+//     numOne++;
+// }
+
+
+
+
+
+// 2. do while loops in javascript
+// let numTwo = 0;
+
+// do {
+//     console.log(numTwo);
+//     numTwo++;
+// } while (numTwo <= 10)
+
+
+
+
+// 3. for loop in javascript 
+
+
+// for (let numOne = 10; numOne <= 20; numOne++) {
+//     console.log(numOne)
+// }

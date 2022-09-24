dd8674e14fb5f5f2633c9bd20d15b6392f3f6711
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2022-09-24 04:52:39 +0000

    type convertion practice

--- 8-type-conversion.js
+++ 8-type-conversion.js
@@ -3,11 +3,31 @@
 // Manual type conversion 
 // The type conversion only used in three data types which are String, number, and boolean. 
 
-let myNumber = "10"; //string data types
+// let myNumber = "10"; //string data types
 
-let numberTwo = 10;
+// let numberTwo = 10;
 
-console.log(Number(myNumber) + numberTwo);
+// console.log(Number(myNumber) + numberTwo);
 
 // ============ TYPE COERCION ========= 
 // AUTOMATIC convert type to another type according to data types
+
+// ================ IMPLICIT TYPE CONVERTION: AUTOMATIC ====================
+// let result;
+// result = '2' + 2;/
+// result = '10' + null;
+// result = '100' + undefined;
+
+// result = 2 + "kailash";
+// result = 2 + undefined;
+// console.log(result);
+
+// ================= EXPLICIT TYPE CONVERTION : MANUAL=====================
+// let stringValue = "Kailash";
+// console.log(typeof stringValue);
+
+// let convertNumber = Number(stringValue);
+// console.log(typeof convertNumber);
+
+// let convertBoolean = Boolean(stringValue);
+// console.log(typeof convertBoolean);
